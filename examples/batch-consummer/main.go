29a6c090697adf5830953ccb61ce539996df37d4
stephane <stephane>
2023-03-26 19:37:23 +0200

    Add example

--- examples/batch-consummer/main.go
+++ examples/batch-consummer/main.go
@@ -11,10 +11,13 @@ import (
 	"time"
 
 	"github.com/colbee1/jobq"
-	jobrepo "github.com/colbee1/jobq/repo/job/memory"
-	pgrepo "github.com/colbee1/jobq/repo/pq/memory"
-	"github.com/colbee1/jobq/services/jqs"
+	job_repo "github.com/colbee1/jobq/repo/job/badger3"
+	pq_repo "github.com/colbee1/jobq/repo/pq/memory"
+	"github.com/colbee1/jobq/service"
+	"github.com/colbee1/jobq/service/jqs"
 )
+
+const Topic = jobq.Topic("demo")
 
 func main() {
 	if err := run(); err != nil {
@@ -24,39 +27,39 @@ func main() {
 }
 
 func run() error {
-	jobRepo, err := jobrepo.New()
+	jobRepo, err := job_repo.New("../../test/db/examples/batch-consummer", job_repo.Options{})
 	if err != nil {
 		return err
 	}
 	defer jobRepo.Close()
 
-	pqRepo, err := pgrepo.New()
+	pqRepo, err := pq_repo.New()
 	if err != nil {
 		return err
 	}
 	defer pqRepo.Close()
 
-	service, err := jqs.New(jobRepo, pqRepo)
+	jqSvc, err := jqs.New(jobRepo, pqRepo)
 	if err != nil {
 		return err
 	}
-	defer service.Close()
+	defer jqSvc.Close()
 
 	// Start the batch consumer
 	//
 	wg := new(sync.WaitGroup)
 	wg.Add(1)
-	go consumer(service, wg)
+	go consumer(jqSvc, wg)
 
 	// Simulate producer
 	//
-	producer(service)
+	producer(jqSvc)
 
 	wg.Wait()
 
 	// List job done
 	//
-	jids, err := service.ListByStatus(context.Background(), jobq.JobStatusDone, 0, 1000)
+	jids, err := jqSvc.ListByStatus(context.Background(), jobq.JobStatusDone, 0, 1000)
 	if err != nil {
 		return err
 	}
@@ -64,20 +67,36 @@ func run() error {
 
 	// List job reserved
 	//
-	jids, err = service.ListByStatus(context.Background(), jobq.JobStatusReserved, 0, 1000)
+	jids, err = jqSvc.ListByStatus(context.Background(), jobq.JobStatusReserved, 0, 1000)
 	if err != nil {
 		return err
 	}
 	fmt.Printf("\n%d jobs are in status Reserved\n", len(jids))
 
+	// List job canceled
+	//
+	jids, err = jqSvc.ListByStatus(context.Background(), jobq.JobStatusCanceled, 0, 1000)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("\n%d jobs are in status Canceled\n", len(jids))
+
+	// List job delayed
+	//
+	jids, err = jqSvc.ListByStatus(context.Background(), jobq.JobStatusDelayed, 0, 1000)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("\n%d jobs are in status Delayed\n", len(jids))
+
 	// Show topic stats
 	//
-	list, err := service.Topics(context.Background(), 0, 100)
+	list, err := jqSvc.Topics(context.Background(), 0, 100)
 	if err != nil {
 		return err
 	}
 	for _, topic := range list {
-		stats, err := service.TopicStats(context.Background(), topic)
+		stats, err := jqSvc.TopicStats(context.Background(), topic)
 		if err != nil {
 			return err
 		}
@@ -87,37 +106,35 @@ func run() error {
 	return nil
 }
 
-func producer(jq jobq.IJobQueueService) {
+func producer(jq service.IJobQueueService) {
 	prng := rand.New(rand.NewSource(time.Now().UnixNano()))
 	upper := math.MaxInt16
 	lower := math.MinInt16
 
 	for i := 0; i < 100; i++ {
 		time.Sleep(time.Duration(prng.Intn(150)) * time.Millisecond)
-		topic := jobq.JobTopic("sendmail")
 
-		priority := jobq.JobPriority(prng.Intn(upper-lower) + lower) // -10 is a higher priority than 10
-		jid, err := jq.Enqueue(context.Background(), topic, priority, jobq.JobOptions{}, jobq.JobPayload("email@domain.tld"))
+		priority := jobq.Priority(prng.Intn(upper-lower) + lower) // -10 is a higher priority than 10
+		jid, err := jq.Enqueue(context.Background(), Topic, priority, jobq.JobOptions{}, jobq.Payload("email@domain.tld"))
 		if err != nil {
 			fmt.Printf("Enqueue error: %v\n", err)
 			continue
 		}
-		fmt.Printf("Job%d was pushed in queue %s with priority %d\n", jid, topic, priority)
+		fmt.Printf("Job%d was pushed in topic %s with priority %d\n", jid, Topic, priority)
 	}
 
 }
 
-func consumer(jq jobq.IJobQueueService, wg *sync.WaitGroup) {
+func consumer(jq service.IJobQueueService, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	topic := jobq.JobTopic("sendmail")
 	batchTicker := time.NewTicker(2 * time.Second)
-	batchSize := 20
+	batchSize := 25
 
 	noMoreJobCounter := 0
 	for noMoreJobCounter < 2 {
 		t := <-batchTicker.C
-		jobs, err := jq.Reserve(context.Background(), topic, batchSize)
+		jobs, err := jq.Reserve(context.Background(), Topic, batchSize)
 		if err != nil {
 			fmt.Printf("Reserve error: %v\n", err)
 			continue
